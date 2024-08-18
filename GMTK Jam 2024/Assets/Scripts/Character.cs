5a85c245d824ecac7d40f94f3e20ccb4ebb69f15
Mateo San Miguel <MS260864@falmouth.ac.uk>
2024-08-18 19:25:25 +0100

    added character scriptable object

---
+++ GMTK Jam 2024/Assets/Scripts/Character.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Character : ScriptableObject
+{
+    public string CharacterName;
+    public Sprite CharacterSprite;
+    public Color CharacterColor;
+}
