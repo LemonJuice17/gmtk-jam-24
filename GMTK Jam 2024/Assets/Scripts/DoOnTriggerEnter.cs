a43eaa586662e5165d02ca001f4d0dfcfc2c98f7
Mateo San Miguel <mateo0404@icloud.com>
2026-04-02 15:01:18 +0100

    More dialogue

--- GMTK Jam 2024/Assets/Scripts/DoOnTriggerEnter.cs
+++ GMTK Jam 2024/Assets/Scripts/DoOnTriggerEnter.cs
@@ -3,19 +3,26 @@ using UnityEngine.Events;
 
 public class DoOnTriggerEnter : MonoBehaviour
 {
-    public bool DisableColliderAfterTriggering = true;
+    public bool DisableCollidersAfterTriggering = true;
 
     public UnityEvent DoStuff = new();
     public void OnTriggerEnter(Collider other)
     {
         if (other.tag != "Player") return;
         DoStuff.Invoke();
-        if (DisableColliderAfterTriggering)
+
+        if (DisableCollidersAfterTriggering)
         {
-            GetComponent<Collider>().enabled = false;
+            Collider[] colliders = GetComponents<Collider>();
+
+            foreach (Collider collider in colliders) 
+            {
+                collider.enabled = false;
+            }
         }
     }
 
+    //Might need updating for multiple colliders?
     public void ReEnable() { if (TryGetComponent(out Collider collider)) collider.enabled = true; }
 
     public void DestroySelf() => Destroy(gameObject);
